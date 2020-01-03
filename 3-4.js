cf9ecbb80b2cae1049d0b76a0b727e6b3182942d
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2020-01-02 17:00:44 -0700

    [3-4] Challenge 2

--- 3-4.js
+++ 3-4.js
@@ -1,9 +1,22 @@
 //Challenge 1
-///*
+/*
  printMsg = (time) => {
     console.log('Hello after ' + time + ' seconds.');
 }
 
 setTimeout(printMsg, 4000, 4);
 setTimeout(printMsg, 8000, 8); 
+//*/
+
+//Chalenge 2
+///*
+var i = 1
+interval = setInterval(() => {
+    console.log('Hello World')
+    i++;
+    if (i > 5) {
+        console.log('Done!')
+        clearTimeout(interval);
+    }
+}, 1000);
 //*/
